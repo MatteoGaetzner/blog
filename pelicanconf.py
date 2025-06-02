0e47b94882fe1afa4130a10aafd76199404b2d0f
Matteo Gaetzner <m.gaetzner@gmx.de>
2025-06-02 12:44:37 +0200

    refactor: more readable font color

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,8 @@ PAGE_URL = "{slug}/"
 PAGE_SAVE_AS = "{slug}/index.html"
 
 DEFAULT_PAGINATION = 10
-THEME = "themes/pelican-alchemy/alchemy"
+# THEME = "themes/pelican-alchemy/alchemy"
+THEME = "theme"
 THEME_CSS_OVERRIDES = ["theme/css/oldstyle.css"]
 SITESUBTITLE = "Projects, insights & thoughts"
 SITEIMAGE = "/images/header.jpg"
@@ -35,27 +36,25 @@ PYGMENTS_STYLE = "monokai"
 HIDE_AUTHORS = True
 
 # Math
-PLUGIN_PATHS = ["pelican-plugins"]
-PLUGINS = ["render_math"]
-RENDER_MATH = True
+# PLUGIN_PATHS = ["pelican-plugins"]
+# PLUGINS = ["render_math"]
+# RENDER_MATH = False
 
-MATH_JAX = {
-    "responsive": True,
-    "process_summary": True,
-    "tex_extensions": ["AMSmath.js", "AMSsymbols.js"],
+# MATH_JAX = {
+#     "responsive": True,
+#     "process_summary": True,
+#     "tex_extensions": ["AMSmath.js", "AMSsymbols.js"],
+#     "MathJax_font": "sanserif",
+# }
+STATIC_PATHS = ["images", "files", "extras"]
+
+EXTRA_PATH_METADATA = {
+    "extras/android-chrome-192x192.png": {"path": "android-chrome-192x192.png"},
+    "extras/android-chrome-512x512.png": {"path": "android-chrome-512x512.png"},
+    "extras/apple-touch-icon.png": {"path": "apple-touch-icon.png"},
+    "extras/favicon-16x16.png": {"path": "favicon-16x16.png"},
+    "extras/favicon-32x32.png": {"path": "favicon-32x32.png"},
+    "extras/favicon.ico": {"path": "favicon.ico"},
+    "extras/manifest.json": {"path": "manifest.json"},
 }
-
-MARKDOWN = {
-    "extensions": [
-        "codehilite",
-        "extra",
-        "mdx_math",  # ← this preserves $…$ and \(...\) math blocks
-        "toc",
-    ],
-    "extension_configs": {
-        "mdx_math": {},  # no extra options needed
-    },
-    "output_format": "html5",
-}
-
-STATIC_PATHS = ["files", "images"]
+RFG_FAVICONS = True
